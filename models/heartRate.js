7ebe82f89adc6ba55ea184f9fbc1d3b0f6b8fc94
Nikossik <nik.grigoriev200547@mail.ru>
2024-05-14 16:12:30 +0300

    [fix] model

--- models/heartRate.js
+++ models/heartRate.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 module.exports = function (sequelize) {
     const HeartRate = sequelize.define('heart_rate', {
         respondentID: Sequelize.INTEGER,
-        testType: Sequelize.ENUM('3_colors', 'light', '3_colors', 'sound', 'math_sound_test', 'math_vis', 'easy_action', 'hard_action', 'analog_tracking_test', 'analog_tracking_test'),
+        testType: Sequelize.ENUM('light', '3_colors', 'sound', 'math_sound_test', 'math_vis', 'easy_action', 'hard_action', 'analog_tracking_test'),
         heartRateBefore: Sequelize.INTEGER,
         heartRateDuring: Sequelize.INTEGER,
         heartRateAfter: Sequelize.INTEGER,
